1a43705d70a43c8f6b184e30fad6a2a6ddef698a
Bruce Makaaru <brucemakallan@gmail.com>
2018-03-30 14:13:10 +0300

    Set up Template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
-	before_filter :set_title
+	before_action :set_title
 
 	def set_title 
 		@page_title = "Elementary School"
